af5a4d00ebe3aa5d0134480a75039bc4fa8e3dbd
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-09-01 12:24:09 +0530

    feat: define Point, TestCase, and Keys types

---
+++ types.go
@@ -0,0 +1,17 @@
+package main
+
+import "math/big"
+
+type Point struct {
+	X, Y *big.Int
+}
+
+type TestCase struct {
+	Keys   Keys                           `json:"keys"`
+	Shares map[string]map[string]string `json:"-"`
+}
+
+type Keys struct {
+	N int `json:"n"`
+	K int `json:"k"`
+}
